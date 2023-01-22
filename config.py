8df7710370707c47045dc472d10f17b020905be4
phoenixtgbot <112406837+phoenixtgbot@users.noreply.github.com>
2023-01-22 20:12:06 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+BOT_TOKEN = "5535947872:AAFNoaKBo6yAPI-8AsKIJbyS3r9LD8tE32c"
+API_ID = 3369707
+SESSION_STRING = "BQCZTjtTA8q8z07bmdRqSuT8FnMh1FWvvZyXxeQ4kejkhTogwIeMrqLez0XhEtWLoGxFycZuTCbzSt4D_5GQhNA1P9X91D9uoiMkUOaRD1CM2bUeEk09O5ip3NEj3MHgN7oGcR913jFfpm44elEYx2pz_WpLu2liCuQKq4_WdLr-gcDrwjdtx3Aq2nEm4BAXkFGEjH6ovkKS7WLcemcfm3RvAb50ySbo4FPtwg7v7sR-LGZ8cucFEV5zzjiPrWABpc80_WLwHQiuxuhbOhyqS5SgrrM5KEliNPGANZLfp3MYUyZryfp5-5eLkzFpoGeAPctASe4WlnwP8MRFDBICE0vVAAAAAEJ2XGcA" # Check Readme to generate sessions; need for userbot
+API_HASH = "aec1fd7abdfec322c426961a570ef336"
+USERBOT_PREFIX = "."
+PHONE_NUMBER = ""  # Need for Userbot
+SUDO_USERS_ID = [
+    1115053159,
+    543214651351,
+]  # Sudo users have full access to everything, don't trust anyone
+LOG_GROUP_ID = -1001315413953
+GBAN_LOG_GROUP_ID = -100125431255
+MESSAGE_DUMP_CHAT = -1001315413953
+WELCOME_DELAY_KICK_SEC = 300
+MONGO_URL = "mongodb+srv://PhoenixKing:PhoenixKing@cluster0.ajwkdee.mongodb.net/?retryWrites=true&w=majority"
+ARQ_API_KEY = "SFGBXP-DFHLZR-BCMCHX-ISCKZJ-ARQ"
+ARQ_API_URL = ""
+LOG_MENTIONS = True
+RSS_DELAY = 300  # In seconds
+PM_PERMIT = True
